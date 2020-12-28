b55ce61e8836d0609d56e8dba8ebaf13ab084f1e
Nikos Katsikanis <nkatsikanis@gmail.com>
2020-12-28 23:56:48 +0200

    Initial commit

---
+++ postcss-tailwind/src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+  const world = 'postcss'; // edit world and save to see hmr update
+</script>
+
+<style>
+  h1 {
+    color: orangered; /* change color an save to see hmr update */
+    /* you can also use css nesting
+    & .world {
+      font-size: 2rem;
+    }
+    */
+  }
+  .world {
+    @apply text-green-500 italic; /*  here's some tailwind apply */
+  }
+</style>
+
+<h1 class="border border-current rounded p-4 m-4">
+  <!-- tailwind classes in svelte template -->
+  Hello
+  <span class="world">{world}</span>
+  <article class="text-sm sm:text-base">
+
+    <textarea
+      autofocus
+      value="test"
+      placeholder="Chat away.. Press ENTER or RETURN on your phone to send. Data will be deleted soon as still in development." />
+
+    <span id="sendInstruction">Press enter to send</span>
+    <p />
+  </article>
+</h1>
